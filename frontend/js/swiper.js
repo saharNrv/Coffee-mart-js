68e655a4bb96604bf4ccc55cb9d7ed79a82bb9bd
saharNrv <122102473+saharNrv@users.noreply.github.com>
2023-09-05 23:24:52 +0330

    update

---
+++ frontend/js/swiper.js
@@ -0,0 +1,13 @@
+
+    var swiper = new Swiper(".mySwiper", {
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+        slidesPerView:3,
+        // breakpoints:{
+        //     460:{
+        //       slidesPerView:2,
+        //     },
+           
+      }
+    });
