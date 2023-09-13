79c0cc2258567601f36d0a1e9816d7b087ab82aa
saharNrv <122102473+saharNrv@users.noreply.github.com>
2023-09-14 00:22:26 +0330

    slider blog

--- frontend/js/swiper.js
+++ frontend/js/swiper.js
@@ -1,9 +1,23 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
-  spaceBetween: 30,
+  spaceBetween: 25,
   freeMode: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    200: {
+      slidesPerView: 1,
+      // spaceBetween: 20,
+    },
+    760: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
   },
 });
