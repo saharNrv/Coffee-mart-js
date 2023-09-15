0cad19d3b795fd2cd1f6deed88e327692a29a600
saharNrv <122102473+saharNrv@users.noreply.github.com>
2023-09-15 23:57:44 +0330

    creat detaile box

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,6 +2,7 @@ const menuBar=document.querySelector('.nav-mobail-hamburg')
 const menuMobail=document.querySelector('.menu-mobail')
 const bg=document.querySelector('.bg')
 const navShopp=document.querySelector('#nav-shopp')
+const navShoppTwo=document.querySelector('#nav-shopp-2')
 const basket=document.querySelector('.basket')
 const basketClose=document.querySelector('.basket-close')
 
@@ -9,7 +10,7 @@ const coffeeBox=document.querySelector('.coffee-box')
 const coffeeImg=document.querySelector('.coffee-img')
 const coffeeLinkBox=document.querySelector('.coffee-link-box')
 
-
+console.log(navShoppTwo);
 
 
 menuBar.addEventListener('click',()=>{
@@ -21,6 +22,12 @@ menuBar.addEventListener('click',()=>{
 
 
 navShopp.addEventListener('click',()=>{
+
+    basket.classList.add('basket-left')
+    bg.classList.toggle('active')
+})
+
+navShoppTwo.addEventListener('click',()=>{
 
     basket.classList.add('basket-left')
     bg.classList.toggle('active')
