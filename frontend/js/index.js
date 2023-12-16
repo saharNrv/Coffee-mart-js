272915d0536115da1cd6a33db04b6e8e90a5f5bb
saharNrv <122102473+saharNrv@users.noreply.github.com>
2023-12-16 23:02:18 +0330

    finsh coffee section js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -12,15 +12,21 @@ const detailClose = document.querySelector('.detail-close')
 
 const coffeeItemsElem = document.querySelectorAll('.coffee-item')
 const coffeeContentWrap = document.querySelector('#coffee-content')
-const detaileImgElem=document.querySelector('.detaile-img')
-const detaileTitleElem=document.querySelector('.detaile-title')
-const detailePriceElem=document.querySelector('.detaile-price')
-const addBtn=document.querySelector('.add-btn')
-const minusBtn=document.querySelector('.minus-btn')
-const inputNumber=document.querySelector('.input-number')
-const detailFormBtn=document.querySelector('.detail-form-btn')
-const basketMenuElem=document.querySelector('.basket-menu')
+const detaileImgElem = document.querySelector('.detaile-img')
+const detaileTitleElem = document.querySelector('.detaile-title')
+const detailePriceElem = document.querySelector('.detaile-price')
+const addBtn = document.querySelector('.add-btn')
+const minusBtn = document.querySelector('.minus-btn')
+const inputNumber = document.querySelector('.input-number')
+const detailFormBtn = document.querySelector('.detail-form-btn')
+const basketMenuElem = document.querySelector('.basket-menu')
+const coffeeItems = document.querySelectorAll('.coffee-item ')
+const allItem = document.querySelector('#all')
+const saleItem=document.querySelector('#sale')
+const specialItem=document.querySelector('#special')
+const selectItem=document.querySelector('#select')
 
+let detailArray = [];
 
 const coffeeBox = document.querySelector('.coffee-box')
 const coffeeImg = document.querySelector('.coffee-img')
@@ -29,8 +35,67 @@ const headerTitle = document.querySelector('.header-infos__title')
 let landingText = 'همه چیزهایی که در مورد قهوه باید بدونید!'
 let indexText = 0
 
+function productCoffee() {
+
+    fetch('http://localhost:3000/products')
+        .then(res => res.json())
+        .then(data => {
+
+            detailArray.push(data)
+            const dataIds = [];
+     let sliceData=data.slice(start,end)
+
+            sliceData.forEach(data => {
+
+                coffeeContentWrap.insertAdjacentHTML('beforeend', `
+           <div class="col-12 col-xs-12 col-md-6 col-lg-3">
+                               <div class="coffee-box" >
+                                   <div class="coffee-img-wrap">
+                                       <a href="#" class="coffee-link-img">
+                                           <img src=${data.img} class="coffee-img">
+                                       </a>
+                                       <div class="coffee-img-wrap-hover">
+                                           <a href="#" class="coffee-link-img">
+                                               <img src=${data.imgBack} class="coffee-img">
+                                           </a>
+                                       </div>
+                                   </div>
+                                   <div class="coffee-btn-wrap">
+                                       <span  class="coffee-icon-link" onClick="basketHnadler(${data.id})">
+                                           <i class="fa-solid fa-cart-shopping"></i>
+                                           </span>
+                                       <span class="coffee-icon-link coffee-icon-link-search coffe-search-js" onClick="searchHandel(${data.id})">
+                                           <i class="fa-solid fa-magnifying-glass"
+                                               id="coffee-icon-link-search"></i>
+                                        </span>
+                                       <span class="coffee-icon-link" onClick="favoritHandler()">
+                                       <i class="fa-regular fa-heart"></i>
+                                       </span>
+                                   </div>
+                                   <div class="coffee-info">
+                                       <h3 class="coffee-info-title"><a href="#" class="coffee-info-title-link">${data.name}</a></h3>
+                                       <span>${data.price} تومان</span>
+                                   </div>
+                               </div>
+                           </div>
+           `)
+
+                dataIds.push(data.id);
+            })
+
+
+
+        })
+}
+
+
+
+
 window.addEventListener('load', () => {
     typeWriter(landingText, indexText)
+    productCoffee()
+    start=0;
+    end=4;
 
 })
 
@@ -82,7 +147,7 @@ bg.addEventListener('click', () => {
     basket.classList.remove('basket-left')
     bg.classList.remove('active')
     detaileBox.classList.toggle('detaile-box-active')
-    inputNumber.value=1
+    inputNumber.value = 1
 
 })
 ///////////////////////
@@ -100,189 +165,153 @@ bg.addEventListener('click', () => {
 detailClose.addEventListener('click', () => {
     detaileBox.classList.toggle('detaile-box-active')
     bg.classList.toggle('active')
-    inputNumber.value=1
+    inputNumber.value = 1
 })
 
 
-// coffeeItemElems.forEach(item=>{
-//     console.log(item);
-//     item.addEventListener('click',function(){
-
-
-//         document.querySelector('.coffee-item-active').classList.remove('coffee-item-active')
-//         document.querySelector('.coffee-content-active').classList.remove('coffee-content-active')
-
-//         this.classList.add('coffee-item-active')
-//         let contentMenu=this.getAttribute('portdata')
-//         console.log(contentMenu);
-//         document.querySelector(contentMenu).classList.add('coffee-content-active')
-
-//         console.log('click');
-//     })
-// })
 // ///////////////////coffee menu
 
 
-let detailArray=[];
 
-fetch('http://localhost:3000/products')
-.then(res=>res.json())
-.then(data=>{
-  
-    detailArray.push(data)
-    const dataIds = [];
-   data.slice(0,4).forEach(data=>{
-
-       coffeeContentWrap.insertAdjacentHTML('beforeend', `
-       <div class="col-12 col-xs-12 col-md-6 col-lg-3">
-                           <div class="coffee-box" >
-                               <div class="coffee-img-wrap">
-                                   <a href="#" class="coffee-link-img">
-                                       <img src=${data.img} class="coffee-img">
-                                   </a>
-                                   <div class="coffee-img-wrap-hover">
-                                       <a href="#" class="coffee-link-img">
-                                           <img src=${data.imgBack} class="coffee-img">
-                                       </a>
-                                   </div>
-                               </div>
-                               <div class="coffee-btn-wrap">
-                                   <span  class="coffee-icon-link" onClick="basketHnadler(${data.id})">
-                                       <i class="fa-solid fa-cart-shopping"></i>
-                                       </span>
-                                   <span class="coffee-icon-link coffee-icon-link-search coffe-search-js" onClick="searchHandel(${data.id})">
-                                       <i class="fa-solid fa-magnifying-glass"
-                                           id="coffee-icon-link-search"></i>
-                                    </span>
-                                   <span class="coffee-icon-link" onClick="favoritHandler()">
-                                   <i class="fa-regular fa-heart"></i>
-                                   </span>
-                               </div>
-                               <div class="coffee-info">
-                                   <h3 class="coffee-info-title"><a href="#" class="coffee-info-title-link">${data.name}</a></h3>
-                                   <span>${data.price} تومان</span>
-                               </div>
-                           </div>
-                       </div>
-       `)
-
-       dataIds.push(data.id);
-   })
-   
-
-      
-})
 
 /*function for post fetch product */
 
-function postProduct(objectPro){
-    fetch('http://localhost:3000/productCart',{
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json"
+function postProduct(objectPro) {
+    fetch('http://localhost:3000/productCart', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
         },
-        body:JSON.stringify(objectPro)
-       })
-       .then(res=>{
-        console.log(res);
-        if(res.status===500){
-            swal({
-               icon:'success',
-               title:'این محصول در سبد خرید قرار دارد',
-              button:'باشه'
-            })
-        }
-       })
+        body: JSON.stringify(objectPro)
+    })
+        .then(res => {
+            console.log(res);
+            if (res.status === 500) {
+                swal({
+                    icon: 'success',
+                    title: 'این محصول در سبد خرید قرار دارد',
+                    button: 'باشه'
+                })
+            }
+        })
 }
 
 
 /* search icon js handel */
-const searchHandel=(id)=>{
-   
+const searchHandel = (id) => {
+
     detaileBox.classList.toggle('detaile-box-active')
     bg.classList.add('active')
-    
 
-    const b=detailArray[0].filter(detail=> detail.id===id )
-   
-    detaileImgElem.setAttribute('src',`${b[0].img}`)
-    detaileTitleElem.innerHTML=`${b[0].name}`
-    detailePriceElem.innerHTML=`${b[0].price} تومان`
 
-    addBtn.addEventListener('click',(e)=>{
+    const b = detailArray[0].filter(detail => detail.id === id)
+
+    detaileImgElem.setAttribute('src', `${b[0].img}`)
+    detaileTitleElem.innerHTML = `${b[0].name}`
+    detailePriceElem.innerHTML = `${b[0].price} تومان`
+
+    addBtn.addEventListener('click', (e) => {
         e.preventDefault()
 
-      let count= inputNumber.value++;
-      
-      let increasePrice=Number(b[0].price)*inputNumber.value
-      detailePriceElem.innerHTML=`${increasePrice}`
-     
+        let count = inputNumber.value++;
+
+        let increasePrice = Number(b[0].price) * inputNumber.value
+        detailePriceElem.innerHTML = `${increasePrice} تومان`
+
     })
-    minusBtn.addEventListener('click',(e)=>{
+    minusBtn.addEventListener('click', (e) => {
         e.preventDefault()
 
 
-        let count= inputNumber.value--;
-        if(inputNumber.value<=1){
-         detailePriceElem.innerHTML=`${b[0].price}`
-         inputNumber.value=1
+        let count = inputNumber.value--;
+        if (inputNumber.value <= 1) {
+            detailePriceElem.innerHTML = `${b[0].price}`
+            inputNumber.value = 1
 
-        }else{
+        } else {
 
-            let increasePrice=Number(b[0].price)*inputNumber.value
-            detailePriceElem.innerHTML=`${increasePrice}`
-        }
-        
-       
-    })
-    
-    detailFormBtn.addEventListener('click',(e)=>{
-      
-        e.preventDefault()
-        let newCartProduct={
-            id:b[0].id,
-            name:b[0].name,
-            price:(b[0].price*inputNumber.value),
-            count:inputNumber.value,
-            img:b[0].img
+            let increasePrice = Number(b[0].price) * inputNumber.value
+            detailePriceElem.innerHTML = `${increasePrice}`
         }
 
-      
-postProduct(newCartProduct)
-      
-       
+
+    })
+
+    detailFormBtn.addEventListener('click', (e) => {
+
+        e.preventDefault()
+        let newCartProduct = {
+            id: b[0].id,
+            name: b[0].name,
+            price: (b[0].price * inputNumber.value),
+            count: inputNumber.value,
+            img: b[0].img
+        }
+
+
+        postProduct(newCartProduct)
+
+
     })
 
 }
 /* basket icon js handler*/
 
-const basketHnadler=(basketID)=>{
-    
-    const b=detailArray[0].filter(detail=> detail.id===basketID )
-   
-    let newCartProduct={
-        id:b[0].id,
-        name:b[0].name,
-        price:(b[0].price*inputNumber.value),
-        count:inputNumber.value,
-        img:b[0].img
+const basketHnadler = (basketID) => {
+
+    const b = detailArray[0].filter(detail => detail.id === basketID)
+
+    let newCartProduct = {
+        id: b[0].id,
+        name: b[0].name,
+        price: (b[0].price * inputNumber.value),
+        count: inputNumber.value,
+        img: b[0].img
     }
 
     postProduct(newCartProduct)
 
 }
 
+coffeeItems.forEach(item => {
+    item.addEventListener('click', function (e) {
+        document.querySelector('.coffee-item-active').classList.remove('coffee-item-active')
 
+        this.classList.add('coffee-item-active')
+    })
 
+})
 
+allItem.addEventListener('click', () => {
+    coffeeContentWrap.innerHTML=''
+    productCoffee()
+start=0;
+end=5;
+})
 
+saleItem.addEventListener('click',()=>{
+    productCoffee()
+    start=2;
+    end=4
 
+})
+specialItem.addEventListener('click',()=>{
+    productCoffee()
+    start=1,
+    end=4
+})
+
+selectItem.addEventListener('click',()=>{
+ productCoffee()
+ start=4;
+ end=5;
+})
 
 fetch('http://localhost:3000/productCart')
-    .then(res=>res.json())
-    .then(data=>{
-        data.map(item=>{
-            basketMenuElem.insertAdjacentHTML('beforeend',`
+    .then(res => res.json())
+    .then(data => {
+        data.map(item => {
+            basketMenuElem.insertAdjacentHTML('beforeend', `
             <li class="basket-item">
             <a href="#" class="basket-link">
             <div class="basket-pro">
@@ -304,16 +333,16 @@ fetch('http://localhost:3000/productCart')
 
 
 coffeeItemsElem.forEach(item => {
-  
+
 
     item.addEventListener('click', (event) => {
         let btnText = event.target.innerHTML;
 
         if (btnText === 'همه') {
-           
-           
+
+
         } else {
-           coffeeContentWrap.innerHTML=''
+            coffeeContentWrap.innerHTML = ''
         }
 
     })
